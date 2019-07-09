95b9d81fd0882bbde865521dd6fa8dda31772def
Sourav Dutta <52704958+TheBengalRed@users.noreply.github.com>
2019-07-09 19:36:47 +0600

    Add files via upload

---
+++ widget.php
@@ -0,0 +1,20 @@
+<h3> <p class="card-text btn btn-info btn-block">Dont Have An Account? Please <a href="registration.php">Registration</a> .</p>
+  
+
+
+
+
+<!-- 
+<h4>Don't Have An Account?</h4>
+                    
+
+
+<a href="registration.php" style="text-decoration: none;"> 
+                                  
+                                  <p class="card-text btn btn-info btn-block" style="color:white;"> 
+                                                                                                  
+                                                                                                  
+                                                                                                  
+                                   Please Register.</p> 
+                                                                                               
+</a> -->
